7d3a64eb333f323dc84e33ecfa7ff1c1bd0a7320
Tarek994 <ghali.tarek.123@hotmail.com>
2023-03-26 19:49:24 +0200

    first lesson: add and style element with javascript

---
+++ main.js
@@ -0,0 +1,5 @@
+// 1 -     how to add a new element with js:
+
+document.write("<h2>hello </h2>");
+document.querySelector("h2").style.color = "red";
+document.querySelector("h2").style.fontSize = "50px";
