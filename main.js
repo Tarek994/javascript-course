733f07e8a13121263232af1b0e1dcb4db5d09d6e
Tarek994 <ghali.tarek.123@hotmail.com>
2023-03-26 20:13:25 +0200

    lesson : where to put the code of javascript

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 // 1 -     how to add a new element with js:
 
-document.write("<h2>hello </h2>");
-document.querySelector("h2").style.color = "red";
-document.querySelector("h2").style.fontSize = "50px";
+// document.write("<h2>hello </h2>");
+// document.querySelector("h2").style.color = "red";
+// document.querySelector("h2").style.fontSize = "50px";
+
+///////////////////////////////////////////////////
+
+// 2 - Where to put the code :
+// we have to put the script in the final the bode tag to make the browser read correctly the commands and apply the changes
+
+// OR we have to set  onload command to make sure the commands are work after load the page:
+
+// window.onload = function (){
+//     document.querySelector("h2").style.color = "red"
+// }
