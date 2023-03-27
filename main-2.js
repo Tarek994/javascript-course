676f4a715e049ed67b6035df15606356463ceea9
Tarek994 <ghali.tarek.123@hotmail.com>
2023-03-27 23:46:30 +0200

    lesson-7 : what is the variable?

--- main-2.js
+++ main-2.js
@@ -9,3 +9,17 @@
         - null
 
 */
+
+/*
+ 2 - variables intro
+        - what is variables?
+        - why we use variables?
+        - declare the variable and use
+        - syntax( keyword: var | variable name: user | assignment operator: = | variable value: "tarek")
+        - variables without var
+        - multiple variables in the same line
+        - Id and global variable
+        - loosely typed vs strongly typed
+
+*/
+
