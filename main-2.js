3072a60c371742185944a3afd7535bb74060e713
Tarek994 <ghali.tarek.123@hotmail.com>
2023-03-27 23:29:40 +0200

    lesson-6 : data types and typeof operator

---
+++ main-2.js
@@ -0,0 +1,11 @@
+/*
+ 1 - Data Types And Typeof Operator:
+        - string
+        - number
+        - array => object
+        - object
+        - boolean
+        - undefined
+        - null
+
+*/
