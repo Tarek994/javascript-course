5bee2ecdbc57eda2f111d68e7c0ed3874cf9678d
Tarek994 <ghali.tarek.123@hotmail.com>
2023-03-31 15:39:30 +0200

    lesson-16: type coercion

---
+++ main-3.js
@@ -0,0 +1,20 @@
+/*
+ 11 - type coercion (type casting)
+    --+
+    ---
+    --"" - 2 
+    -- false - true
+
+    let a = "10";
+    let b = 20;
+    let c = true;
+    
+    console.log(+a + b + true); // result = 31
+
+
+    note :
+    false = 0
+    true = 1
+
+
+*/
