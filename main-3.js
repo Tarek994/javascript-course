e45ef8c2468d30a174a19fd4a6c1ad32dcdb70e4
Tarek994 <ghali.tarek.123@hotmail.com>
2023-04-11 23:59:38 +0200

    lesson-18: Operators Challenges

--- main-3.js
+++ main-3.js
@@ -30,4 +30,20 @@
     a /= 50; // a = a / 50
 
     console.log(a);
+    
+*/
+
+/*
+
+13 - Operators Challenges
+
+     let a = 10;
+     let b = "20";
+     let c = 80;
+
+     console.log (++a + +b++ + +c++ + +a++)
+     console.log (++a + -b + +c++ - -a++ + +a)
+     console.log (--c + +b + --a * +b++ - +b * a + --a - +true)
+
+
 */
